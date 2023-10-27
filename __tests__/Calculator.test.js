d5053e5793863616fd40ff64c2225a8ca019ed76
Maciej Necka <macieknecka@gmail.com>
2023-10-27 16:20:53 +0200

    106/133

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,5 +1,4 @@
 'use strict';
-const Calculator = require('./../src/Calculator');
 // test('1 + 1 = 2', () => {
 //   expect(1 + 1).toBe(2);
 // });
@@ -17,8 +16,51 @@ const Calculator = require('./../src/Calculator');
 // test('3 + 2 = 5', () => {
 //   expect(add(3, 2)).toBe(5);
 // });
+const Calculator = require('./../src/Calculator');
 
-it('return 0 when prop num1 is not set', () => {
-  const calc = new Calculator();
-  expect(calc.num1).toBe(0);
+describe('Calculator', () => {
+  describe('Prop num1', () => {
+    it('return 0 when prop is not set', () => {
+      const calc = new Calculator();
+      expect(calc.num1).toBe(0);
+    });
+    it('return 4 when prop is set to 4 in constructor', () => {
+      const value = 4;
+      const calc = new Calculator(value);
+      expect(calc.num1).toBe(value);
+    });
+    it('throw expection when prop is not a number', () => {
+      function createCalculator() {
+        new Calculator('4');
+      }
+      expect(createCalculator).toThrow();
+    });
+  });
+
+  describe('Prop num2', () => {
+    it('return 0 when prop is not set', () => {
+      const calc = new Calculator();
+      expect(calc.num2).toBe(0);
+    });
+    it('return 4 when prop is set to 4 in constructor', () => {
+      const value = 4;
+      const calc = new Calculator(undefined, value);
+      expect(calc.num2).toBe(value);
+    });
+
+    it('throw expection when prop is not a number', () => {
+      function createCalculator() {
+        new Calculator(undefined, '4');
+      }
+      expect(createCalculator).toThrow();
+    });
+  });
+
+  describe('Method add()', () => {
+    it('return 7 when num1 = 4 and num2 = 3', () => {
+      const calc = new Calculator(4, 3);
+      const result = calc.add();
+      expect(result).toBe(7);
+    });
+  });
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,18 @@
 class Calculator {
-  constructor() {
-    this.num1 = 0;
+  constructor(num1Value = 0, num2Value = 0) {
+    this.setProp('num1', num1Value);
+    this.setProp('num2', num2Value);
+  }
+
+  setProp(propName, value) {
+    if (typeof value !== 'number') {
+      throw new Error('Property have to be a number');
+    }
+    this[propName] = value;
+  }
+  add() {
+    const { num1, num2 } = this;
+    return num1 + num2;
   }
 }
 
